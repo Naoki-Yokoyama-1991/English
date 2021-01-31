c6568683aba04dc7433429ff94065a6f5fcd894f
Naoki-Yokoyama-1991 <naoki.yokoyama.1991@gmail.com>
2021-01-31 18:38:25 +0200

    create user

--- app/controllers/user.go
+++ app/controllers/user.go
@@ -1,8 +1,6 @@
 package controllers
 
 import (
-	"io/ioutil"
-
 	"github.com/gin-gonic/gin"
 	"github.com/naoki/gacha/app/api"
 )
@@ -11,9 +9,11 @@ var server = api.Server{}
 
 func CreateUser(c *gin.Context) {
 
+	/* this is here
 	errList := map[string]string{}
 	body, err := ioutil.ReadAll(c.Request.Body)
 	if err != nil {
 		errList["Invalid_body"] = "Unable to get request"
 	}
+	*/
 }
